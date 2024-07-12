25477840b437f1b1b252f7ad39dcac3a7e80891f
Markovich Gdl <136863287+markovichgdl@users.noreply.github.com>
2024-07-11 18:33:14 -0600

    host2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5896;
 const uploadDir = process.env.UPLOAD_DIR || "uploads";
 
 // Asegúrate de que el directorio de subidas existe
